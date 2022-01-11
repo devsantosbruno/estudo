bbe8647a0e54d1a845e1945a9dd50869ce7ee903
bruno santos <devbrunosantos@gmail.com>
2022-01-11 08:40:03 -0300

    terceira forma

--- sequence.js
+++ sequence.js
@@ -1,46 +1,9 @@
-var numbers = []
-
-// primeira forma
-// for (n = 0; n < 8; n++) {
-//   let ip = n % 2
-//   if (ip != 0) {
-//     numbers.push(n)
-//   }
-// }
-
-// let par = numbers.length * 2
-// numbers.push(par)
-
-// console.log('Teste:')
-// console.log(numbers)
-
-// console.log('\nSolução:')
-// for (x = 4; x >= 0; x--) {
-//   console.log(numbers[x])
-// }
-
-// segunda forma
-for (n = 0; n < 8; n++) {
-  let ip = n % 2
-  if (ip != 0) {
-    numbers.push(n)
-  }
-}
-let par1 = numbers.length * 2
-numbers.push(par1)
-
+var numbers = [1, 3, 5, 7, 8]
 console.log('Teste:')
 console.log(numbers)
 
-numbers = []
-
-for (n = 0; n < 8; n++) {
-  let ip = n % 2
-  if (ip != 0) {
-    numbers.unshift(n)
-  }
-}
-let par2 = numbers.length * 2
-numbers.unshift(par2)
 console.log('\nSolução:')
-console.log(numbers)
+console.log(8)
+for (x = 7; x > 0; x -= 2) {
+  console.log(x)
+}
